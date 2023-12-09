232f93ae454e49ee67a6fad10d3bff8267989168
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 15:59:11 +0100

    FEAT:: add check function

--- gsheets/authentication/authentication.py
+++ gsheets/authentication/authentication.py
@@ -1,4 +1,7 @@
-import os
+from pathlib import Path
+
+CREDENTIALS_FILE_NAME = "credentials.json"
+TOKEN_FILE_NAME = "token.json"
 
 
 class Authentication:
@@ -6,7 +9,17 @@ class Authentication:
     Authentication class for Google Sheets API
     """
 
-    def __init__(self, path_to_credentials):
-        if not os.path.exists(path_to_credentials):
+    def __init__(self, credentials_folder):
+        if not folder_contains_credentials_file(credentials_folder):
             raise FileNotFoundError
-        self.path_to_credentials = path_to_credentials
+        self.path_to_credentials = Path(credentials_folder).joinpath(
+            CREDENTIALS_FILE_NAME
+        )
+        self.path_to_token = Path(credentials_folder).joinpath(TOKEN_FILE_NAME)
+
+    def check(self):
+        print("Authentication OK")
+
+
+def folder_contains_credentials_file(credentials_folder):
+    return Path(credentials_folder).joinpath(CREDENTIALS_FILE_NAME).is_file()

d27a2b414d4ce2e7e2cfe76584a042e860af7f40
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 13:41:21 +0100

    FEAT:: check if credentials file exists

--- gsheets/authentication/authentication.py
+++ gsheets/authentication/authentication.py
@@ -1,8 +1,12 @@
+import os
+
+
 class Authentication:
     """
     Authentication class for Google Sheets API
     """
 
     def __init__(self, path_to_credentials):
+        if not os.path.exists(path_to_credentials):
+            raise FileNotFoundError
         self.path_to_credentials = path_to_credentials
-        raise FileNotFoundError

9980d90740a887952a909e3e9ef537217bdf933b
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 14:29:00 +0100

    TEST:: add call checks to test cases

--- gsheets/authentication/tests/test_authentication.py
+++ gsheets/authentication/tests/test_authentication.py
@@ -13,6 +13,8 @@ def test_start_without_credentials(mocked_path_exists):
         # When
         auth = Authentication(path_to_credentials="not_existing_credentials.json")
 
+    mocked_path_exists.assert_called_once_with("not_existing_credentials.json")
+
 
 @patch("authentication.authentication.os.path.exists")
 def test_start_with_credentials(mocked_path_exists):
@@ -23,4 +25,5 @@ def test_start_with_credentials(mocked_path_exists):
     auth = Authentication(path_to_credentials="some_credentials.json")
 
     # Then
+    mocked_path_exists.assert_called_once_with("some_credentials.json")
     assert auth.path_to_credentials == "some_credentials.json"

f7b4254114eb4599a61a978e9f4fc7743deb835e
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 17:57:00 +0100

    TEST:: test cases to check multiple credential types

--- gsheets/authentication/tests/test_authentication.py
+++ gsheets/authentication/tests/test_authentication.py
@@ -1,11 +1,10 @@
 import pytest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, MagicMock
 from pathlib import Path
 from authentication.authentication import (
     Authentication,
     folder_contains_credentials_file,
     folder_contains_token_file,
-    token_is_valid,
 )
 
 
@@ -77,14 +76,22 @@ def test_check_with_valid_token(auth_fixture):
     test_auth = auth_fixture[0]
     token_path = auth_fixture[1].joinpath("token.json")
 
-    with patch(
-        "authentication.authentication.folder_contains_token_file"
-    ) as mocked_folder_contains_token_file:
-        mocked_folder_contains_token_file.return_value = True
+    with patch("authentication.authentication.Credentials") as MockCredentials:
+        mock_credentials = MagicMock()
+        mock_credentials.expired = False
+        mock_credentials.refresh_token = False
+        mock_credentials.valid = True
 
-        with patch("builtins.print") as mocked_print:
-            # when
-            return_value = test_auth.check()
+        MockCredentials.from_authorized_user_file.return_value = mock_credentials
+
+        with patch(
+            "authentication.authentication.folder_contains_token_file"
+        ) as mocked_folder_contains_token_file:
+            mocked_folder_contains_token_file.return_value = True
+
+            with patch("builtins.print") as mocked_print:
+                # when
+                return_value = test_auth.check()
 
     # Then
     mocked_print.assert_called_once_with("Authentication OK")
@@ -95,25 +102,51 @@ def test_check_with_valid_token(auth_fixture):
 def test_check_with_invalid_token(auth_fixture):
     # Given
     test_auth = auth_fixture[0]
-    token_path = auth_fixture[1].joinpath("token.json")
 
-    with patch(
-        "authentication.authentication.folder_contains_token_file"
-    ) as mocked_folder_contains_token_file:
-        mocked_folder_contains_token_file.return_value = True
+    with patch("authentication.authentication.Credentials") as MockCredentials:
+        mock_credentials = MagicMock()
+        mock_credentials.expired = True
+        mock_credentials.refresh_token = False
+        mock_credentials.valid = False
+
+        MockCredentials.from_authorized_user_file.return_value = mock_credentials
 
         with patch(
-            "authentication.authentication.token_is_valid"
-        ) as mocked_token_is_valid:
-            mocked_token_is_valid.return_value = False
+            "authentication.authentication.folder_contains_token_file"
+        ) as mocked_folder_contains_token_file:
+            mocked_folder_contains_token_file.return_value = True
 
             # When
             return_value = test_auth.check()
 
     # Then
-    mocked_folder_contains_token_file.assert_called_once_with(token_path)
-    mocked_token_is_valid.assert_called_once_with(token_path)
     assert return_value is False
+
+
+def test_check_with_refreshable_token(auth_fixture):
+    # Given
+    test_auth = auth_fixture[0]
+
+    with patch("authentication.authentication.Credentials") as MockCredentials:
+        mock_credentials = MagicMock()
+        mock_credentials.expired = True
+        mock_credentials.refresh_token = True
+        mock_credentials.valid = False
+        mock_credentials.refresh = MagicMock()
+
+        MockCredentials.from_authorized_user_file.return_value = mock_credentials
+
+        with patch(
+            "authentication.authentication.folder_contains_token_file"
+        ) as mocked_folder_contains_token_file:
+            mocked_folder_contains_token_file.return_value = True
+
+            # When
+            return_value = test_auth.check()
+
+    # Then
+    mock_credentials.refresh.assert_called_once()
+    assert return_value is True
 
 
 def test_check_with_not_existing_token(auth_fixture):

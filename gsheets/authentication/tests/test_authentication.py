5bff9b90f3f7d4d63c489d9e2c13745d0b136ac5
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 13:34:18 +0100

    TEST:: add test case to initialization check

--- gsheets/authentication/tests/test_authentication.py
+++ gsheets/authentication/tests/test_authentication.py
@@ -7,4 +7,12 @@ def test_first_log_in_without_credentials():
     # Then
     with pytest.raises(FileNotFoundError):
         # When
-        auth = Authentication(path_to_credentials="credentials.json")
+        auth = Authentication(path_to_credentials="not_existing_credentials.json")
+
+
+def test_first_log_in_with_credentials():
+    # When
+    auth = Authentication(path_to_credentials="some_credentials.json")
+
+    # Then
+    assert auth.path_to_credentials == "some_credentials.json"

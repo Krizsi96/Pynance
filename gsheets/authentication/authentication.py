7965ea4965c4cf989c23365d73e193d42ee67d70
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 13:27:15 +0100

    TEST:: add test case for Authetication class

---
+++ gsheets/authentication/authentication.py
@@ -0,0 +1,7 @@
+class Authentication:
+    """
+    Authentication class for Google Sheets API
+    """
+
+    def __init__(self, path_to_credentials):
+        self.path_to_credentials = path_to_credentials

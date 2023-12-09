4d3f841744b1d171a09b632b2bddcb1ce3bc3040
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 16:04:16 +0100

    TEST:: add test case for non existing token file

--- gsheets/authentication/tests/test_authentication.py
+++ gsheets/authentication/tests/test_authentication.py
@@ -54,3 +54,23 @@ def test_check_with_valid_token():
 
     # Then
     mocked_print.assert_called_once_with("Authentication OK")
+
+
+@patch("authentication.authentication.folder_contains_token_file")
+def test_check_with_not_existing_token(mocked_folder_contains_token_file):
+    # Given
+    with patch(
+        "authentication.authentication.folder_contains_credentials_file"
+    ) as mocked_folder_contains_credentials_file:
+        mocked_folder_contains_credentials_file.return_value = True
+        test_auth = Authentication(credentials_folder="/path/to/existing/credentials")
+        test_auth.path_to_token = Mock(is_file=Mock(return_value=False))
+
+    mocked_folder_contains_token_file.return_value = False
+
+    # When
+    test_auth.check()
+
+    mocked_folder_contains_token_file.assert_called_once_with(
+        "/path/to/existing/credentials/token.json"
+    )

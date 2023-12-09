e97b20d50c3def3688ecb1c812b8364836d957d8
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2023-12-09 17:01:37 +0100

    TEST:: add test case and refactor

--- gsheets/authentication/tests/test_authentication.py
+++ gsheets/authentication/tests/test_authentication.py
@@ -62,17 +62,21 @@ def test_folder_contains_token_file(mocked_path):
 
 
 @pytest.fixture
-def test_auth():
+def auth_fixture():
     with patch(
         "authentication.authentication.folder_contains_credentials_file"
     ) as mocked_folder_contains_credentials_file:
         mocked_folder_contains_credentials_file.return_value = True
+        test_auth = Authentication(credentials_folder="/path/to/existing/credentials")
+        credentials_folder = Path("/path/to/existing/credentials")
+        return test_auth, credentials_folder
 
-        return Authentication(credentials_folder="/path/to/existing/credentials")
 
-
-def test_check_with_valid_token(test_auth):
+def test_check_with_valid_token(auth_fixture):
     # Given
+    test_auth = auth_fixture[0]
+    token_path = auth_fixture[1].joinpath("token.json")
+
     with patch(
         "authentication.authentication.folder_contains_token_file"
     ) as mocked_folder_contains_token_file:
@@ -84,14 +88,39 @@ def test_check_with_valid_token(test_auth):
 
     # Then
     mocked_print.assert_called_once_with("Authentication OK")
-    mocked_folder_contains_token_file.assert_called_once_with(
-        Path("/path/to/existing/credentials/token.json")
-    )
+    mocked_folder_contains_token_file.assert_called_once_with(token_path)
     assert return_value is True
 
 
-def test_check_with_not_existing_token(test_auth):
+def test_check_with_invalid_token(auth_fixture):
     # Given
+    test_auth = auth_fixture[0]
+    token_path = auth_fixture[1].joinpath("token.json")
+
+    with patch(
+        "authentication.authentication.folder_contains_token_file"
+    ) as mocked_folder_contains_token_file:
+        mocked_folder_contains_token_file.return_value = True
+
+        with patch(
+            "authentication.authentication.token_is_valid"
+        ) as mocked_token_is_valid:
+            mocked_token_is_valid.return_value = False
+
+            # When
+            return_value = test_auth.check()
+
+    # Then
+    mocked_folder_contains_token_file.assert_called_once_with(token_path)
+    mocked_token_is_valid.assert_called_once_with(token_path)
+    assert return_value is False
+
+
+def test_check_with_not_existing_token(auth_fixture):
+    # Given
+    test_auth = auth_fixture[0]
+    token_path = auth_fixture[1].joinpath("token.json")
+
     with patch(
         "authentication.authentication.folder_contains_token_file"
     ) as mocked_folder_contains_token_file:
@@ -101,7 +130,5 @@ def test_check_with_not_existing_token(test_auth):
         return_value = test_auth.check()
 
     # Then
-    mocked_folder_contains_token_file.assert_called_once_with(
-        Path("/path/to/existing/credentials/token.json")
-    )
+    mocked_folder_contains_token_file.assert_called_once_with(token_path)
     return_value is False

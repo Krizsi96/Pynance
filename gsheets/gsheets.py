e47c6121f202a0d8fe179bc135a7605f1d18d5f0
Kristof Kovacs <A0075075@kiongroup.com>
2023-12-19 14:37:11 +0100

    FEAT:: create cli for gsheets

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python3
 from authentication.authentication import Authentication
+import click
 import logging
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
-)
 
-logging.info("Starting gsheets")
-try:
-    user = Authentication("/home/kristof/Pynance/gsheets")
-except FileNotFoundError as error:
-    logging.error(error)
-    exit(1)
+@click.group()
+def cli():
+    pass
 
 
-if not user.check_credentials():
+@click.command()
+def run():
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    logging.info("Starting gsheets")
+
     try:
-        user.login()
-        user.save_credentials()
-    except ValueError as error:
+        user = Authentication("/home/kristof/Pynance/gsheets")
+    except FileNotFoundError as error:
         logging.error(error)
         exit(1)
 
-user.load_credentials()
+    user = authentication(user)
+
+
+def authentication(user):
+    if not user.check_credentials():
+        try:
+            user.login()
+            user.save_credentials()
+        except ValueError as error:
+            logging.error(error)
+            exit(1)
+
+    user.load_credentials()
+
+    return user
+
+
+cli.add_command(run)
+
+if __name__ == "__main__":
+    cli()

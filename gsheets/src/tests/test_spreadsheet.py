7f8ca0ed6a0919fb2a08843f874c5bb0e1f3045d
Kristof Kovacs <A0075075@kiongroup.com>
2023-12-19 15:37:17 +0100

    TEST:: add test case to check created service

--- gsheets/src/tests/test_spreadsheet.py
+++ gsheets/src/tests/test_spreadsheet.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from src.spreadsheet import Spreadsheet
 
 
@@ -14,3 +14,18 @@ def test_spreadsheet_id_is_stored_initialization(mocked_build):
 
     # Then
     assert spreadsheet.spreadsheet_id == spreadsheet_id
+
+
+@patch("src.spreadsheet.build")
+def test_spreadsheet_service_is_created_during_initialization(mocked_build):
+    # Given
+    mocked_service = MagicMock()
+    mocked_build.return_value = mocked_service
+    credentials = "credentials"
+
+    # When
+    spreadsheet = Spreadsheet("spreadsheet_id", credentials)
+
+    # Then
+    mocked_build.assert_called_once_with("sheets", "v4", credentials=credentials)
+    assert spreadsheet.service == mocked_service

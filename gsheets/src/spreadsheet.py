5186d5599e4379b9191225960c54efd07c39d4b2
Kristof Kovacs <A0075075@kiongroup.com>
2023-12-19 15:38:06 +0100

    REFACTOR:: add type description to init

--- gsheets/src/spreadsheet.py
+++ gsheets/src/spreadsheet.py
@@ -1,8 +1,9 @@
 from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
 
 
 class Spreadsheet:
-    def __init__(self, spreadsheet_id: str, credentials):
+    def __init__(self, spreadsheet_id: str, credentials: Credentials):
         self.spreadsheet_id = spreadsheet_id
         self.service = build("sheets", "v4", credentials=credentials)
 

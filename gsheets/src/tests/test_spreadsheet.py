1e9d32f433b2fafb7a13cd6ae68a6cdc76d80754
Kristof Kovacs <A0075075@kiongroup.com>
2023-12-19 17:10:28 +0100

    REFACTOR:: test cases

--- gsheets/src/tests/test_spreadsheet.py
+++ gsheets/src/tests/test_spreadsheet.py
@@ -4,13 +4,12 @@ from src.spreadsheet import Spreadsheet
 
 
 @patch("src.spreadsheet.build")
-def test_spreadsheet_id_is_stored_initialization(mocked_build):
+def test_spreadsheet_id_is_stored_during_initialization(mocked_build):
     # Given
     spreadsheet_id = "spreadsheet_id"
-    credentials = "credentials"
 
     # When
-    spreadsheet = Spreadsheet(spreadsheet_id, credentials)
+    spreadsheet = Spreadsheet(spreadsheet_id, "credentials")
 
     # Then
     assert spreadsheet.spreadsheet_id == spreadsheet_id

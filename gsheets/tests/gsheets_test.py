e8a695fc4843492350926f32a40376005e3ebc58
Kristof Kovacs <A0075075@kiongroup.com>
2023-12-20 17:21:57 +0100

    TEST:: add test case for client creation

--- gsheets/tests/gsheets_test.py
+++ gsheets/tests/gsheets_test.py
@@ -1,4 +1,4 @@
-from gsheets.gsheets import configure_logging
+from gsheets.gsheets import configure_logging, create_client
 from unittest.mock import patch
 import pytest
 import logging
@@ -13,3 +13,17 @@ def test_logging_configuration(mocked_logging_basicConfig):
     assert mocked_logging_basicConfig.called_once_with(
         format="%(asctime)s - %(levelname)s - %(message)s", level=logging.DEBUG
     )
+
+
+@patch("gsheets.gsheets.gspread.service_account")
+def test_client_creation(mocked_service_account):
+    # Given
+    filename = "filename"
+    mocked_service_account.return_value = "new_client"
+
+    # When
+    return_value = create_client(filename)
+
+    # Then
+    assert mocked_service_account.called_once_with(filename)
+    assert return_value == "new_client"

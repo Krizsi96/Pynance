87105709bb3539cc211b33a3462fbb7c9dc4cc98
Kristof Kovacs <A0075075@kiongroup.com>
2023-12-20 16:29:00 +0100

    FEAT:: add argument for spreadsheet name

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -10,20 +10,36 @@ def cli():
 
 
 @click.command()
+@click.argument("spreadsheet_name", type=str, required=True)
 @click.argument("range_of_cells", type=str, required=True)
 @click.argument("value", type=str, required=True)
-def update(range_of_cells, value):
+def update(spreadsheet_name, range_of_cells, value):
+    configure_logging()
+    logging.info(f"Updating spreadsheet {spreadsheet_name}")
     client = gspread.service_account(
         filename="/home/kristof/Pynance/gsheets/service_account.json"
     )
-    spreadsheet = client.open("PynanceTest")
+    try:
+        spreadsheet = client.open(spreadsheet_name)
+        logging.debug(f"Spreadsheet {spreadsheet_name} opened")
+    except gspread.SpreadsheetNotFound:
+        logging.error(f"Spreadsheet {spreadsheet_name} not found")
+        return
     worksheet = spreadsheet.sheet1
-
+    logging.debug(f"Worksheet {worksheet.title} opened")
     cell_list = worksheet.range(range_of_cells)
     for cell in cell_list:
         cell.value = value
-
     worksheet.update_cells(cell_list)
+    logging.info(
+        f"Cell(s) '{worksheet.title}!{range_of_cells}' updated with value '{value}'"
+    )
+
+
+def configure_logging():
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(message)s", level=logging.DEBUG
+    )
 
 
 cli.add_command(update)

8a96f7f78d3c4864de5c2473292b1db5e0b354e3
Kristof Kovacs <A0075075@kiongroup.com>
2023-12-21 11:39:19 +0100

    FEAT:: read and print values out from a given spreadsheet

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -32,6 +32,25 @@ def update(spreadsheet_name, range_of_cells, value):
     )
 
 
+@click.command()
+@click.argument("spreadsheet_name", type=str, required=True)
+@click.argument("range_of_cells", type=str, required=True)
+def read(spreadsheet_name, range_of_cells):
+    configure_logging()
+    logging.info(f"Reading spreadsheet {spreadsheet_name}")
+    try:
+        client = create_client("/home/kristof/Pynance/gsheets/service_account.json")
+        spreadsheet = open_spreadsheet(client, spreadsheet_name)
+    except [gspread.SpreadsheetNotFound, FileNotFoundError]:
+        return
+    worksheet = spreadsheet.sheet1
+    logging.debug(f"Worksheet {worksheet.title} opened")
+    cell_list = worksheet.range(range_of_cells)
+    for cell in cell_list:
+        print(cell.value)
+    logging.info(f"Cell(s) '{worksheet.title}!{range_of_cells}' read")
+
+
 def configure_logging():
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(message)s", level=logging.DEBUG
@@ -59,6 +78,7 @@ def open_spreadsheet(client, spreadsheet_name):
 
 
 cli.add_command(update)
+cli.add_command(read)
 
 if __name__ == "__main__":
     cli()
